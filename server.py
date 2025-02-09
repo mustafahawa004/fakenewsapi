9c10fa6b371b18597a9e445666e978fb5633b00f
mustafahawa004 <mustafahawa004@gmail.com>
2025-02-09 00:20:04 -0500

    First commit

---
+++ server.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, request, jsonify
+import pickle
+
+app = Flask(__name__)
+
+with open("fake_news_model.pkl", "rb") as file:
+    model = pickle.load(file)
+
+@app.route('/')
+def home():
+    return "Fake News Detection API is running"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    news_text = data.get("text", "")
+
+    if not news_text.strip():
+        return jsonify(["error:" "Empty text input!"]), 400
+    
+    prediction = model.predict([news_text])[0]
+    result = "Fake News" if prediction == 1 else "Real News"
+
+    return jsonify({"prediction": result})
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
