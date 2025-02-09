2bb02ce936f79e3cc95d45df850898b7c918859f
mustafahawa004 <mustafahawa004@gmail.com>
2025-02-09 01:34:47 -0500

    Added Flask-Cors

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-import os
 from flask import Flask, request, jsonify
 import pickle
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 with open("fake_news_model.pkl", "rb") as file:
     model = pickle.load(file)
@@ -25,4 +26,4 @@ def predict():
     return jsonify({"prediction": result})
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
